5463b62886abfa39f1870f3386a636820f20084a
MiyazakiTakumi-724 <miyataku724@gmail.com>
2026-06-16 02:27:27 +0900

    プロジェクト管理画面のUI完成

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-export const metadata = {
-  title: '旅行プロジェクト',
-};
+"use client";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center pt-10 bg-gray-50">
       <p className="absolute top-20 font-bold text-8xl">
-        旅行プロジェクト
+        旅行割り勘サイト
       </p>
       <button className="bg-blue-950 text-white px-12 py-8 rounded-full text-5xl font-bold">
         ログイン
